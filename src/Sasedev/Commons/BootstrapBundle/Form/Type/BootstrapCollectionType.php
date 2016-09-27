bd4452091c377a2577bf5d1acbda8c4ab52d678e
sasedev <sasedev.bifidis@gmail.com>
2016-09-27 10:56:03 +0200

    test

--- src/Sasedev/Commons/BootstrapBundle/Form/Type/BootstrapCollectionType.php
+++ src/Sasedev/Commons/BootstrapBundle/Form/Type/BootstrapCollectionType.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Sasedev\Commons\BootstrapBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
@@ -16,74 +15,90 @@ use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 class BootstrapCollectionType extends AbstractType
 {
 
-	/**
-	 *
-	 * {@inheritDoc} @see AbstractType::buildView()
-	 */
-	public function buildView(FormView $view, FormInterface $form, array $options)
-	{
-		$view->vars = array_replace($view->vars,
-			array('allow_add' => $options['allow_add'], 'allow_delete' => $options['allow_delete'],
-				'add_button_text' => $options['add_button_text'], 'delete_button_text' => $options['delete_button_text'],
-				'sub_widget_col' => $options['sub_widget_col'], 'button_col' => $options['button_col'],
-				'prototype_name' => $options['prototype_name'], 'add_icon' => $options['add_icon'], 'delete_icon' => $options['delete_icon']));
+  /**
+   *
+   * {@inheritdoc} @see AbstractType::buildView()
+   */
+  public function buildView(FormView $view, FormInterface $form, array $options)
+  {
+    $view->vars = array_replace($view->vars, array(
+      'allow_add' => $options['allow_add'],
+      'allow_delete' => $options['allow_delete'],
+      'add_button_text' => $options['add_button_text'],
+      'delete_button_text' => $options['delete_button_text'],
+      'sub_widget_col' => $options['sub_widget_col'],
+      'button_col' => $options['button_col'],
+      'prototype_name' => $options['prototype_name'],
+      'add_icon' => $options['add_icon'],
+      'delete_icon' => $options['delete_icon']
+    ));
 
-		if (false === $view->vars['allow_delete']) {
-			$view->vars['sub_widget_col'] += $view->vars['button_col'];
-		}
+    if (false === $view->vars['allow_delete']) {
+      $view->vars['sub_widget_col'] += $view->vars['button_col'];
+    }
 
-		if ($form->getConfig()->hasAttribute('prototype')) {
-			$view->vars['prototype'] = $form->getConfig()
-				->getAttribute('prototype')
-				->createView($view);
-		}
-	}
+    if ($form->getConfig()->hasAttribute('prototype')) {
+      $view->vars['prototype'] = $form->getConfig()
+        ->getAttribute('prototype')
+        ->createView($view);
+    }
+  }
 
-	/**
-	 * {@inheritDoc} @see AbstractType::configureOptions()
-	 */
-	public function configureOptions(OptionsResolver $resolver)
-	{
-		$optionsNormalizer = function (Options $options, $value)
-		{
-			// @codeCoverageIgnoreStart
-			$value['block_name'] = 'entry';
+  /**
+   *
+   * {@inheritdoc} @see AbstractType::configureOptions()
+   */
+  public function configureOptions(OptionsResolver $resolver)
+  {
+    $optionsNormalizer = function (Options $options, $value) {
+      // @codeCoverageIgnoreStart
+      $value['block_name'] = 'entry';
 
-			return $value;
-			// @codeCoverageIgnoreEnd
-		};
+      return $value;
+      // @codeCoverageIgnoreEnd
+    };
 
-		$resolver->setDefaults(
-			array('allow_add' => false, 'allow_delete' => false, 'prototype' => true, 'prototype_name' => '__name__', 'type' => 'text',
-				'add_button_text' => 'Add', 'add_icon' => 'plus', 'delete_button_text' => 'Delete', 'delete_icon' => 'trash',
-				'sub_widget_col' => 9, 'button_col' => 3, 'options' => array()));
+    $resolver->setDefaults(array(
+      'allow_add' => false,
+      'allow_delete' => false,
+      'prototype' => true,
+      'prototype_name' => '__name__',
+      'type' => 'text',
+      'add_button_text' => 'Add',
+      'add_icon' => 'plus',
+      'delete_button_text' => 'Delete',
+      'delete_icon' => 'trash',
+      'sub_widget_col' => 9,
+      'button_col' => 3,
+      'options' => array()
+    ));
 
-		$resolver->setNormalizers(array('options' => $optionsNormalizer));
-	}
+    $resolver->setNormalizer('options', $optionsNormalizer);
+  }
 
-	/**
-	 *
-	 * {@inheritDoc} @see AbstractType::getParent()
-	 */
-	public function getParent()
-	{
-		return CollectionType::class;
-	}
+  /**
+   *
+   * {@inheritdoc} @see AbstractType::getParent()
+   */
+  public function getParent()
+  {
+    return CollectionType::class;
+  }
 
-	/**
-	 * return Name
-	 */
-	public function getName()
-	{
-		return 'bootstrap_collection';
-	}
+  /**
+   * return Name
+   */
+  public function getName()
+  {
+    return 'bootstrap_collection';
+  }
 
-	/**
-	 *
-	 * {@inheritDoc} @see AbstractType::getBlockPrefix()
-	 */
-	public function getBlockPrefix()
-	{
-		return $this->getName();
-	}
+  /**
+   *
+   * {@inheritdoc} @see AbstractType::getBlockPrefix()
+   */
+  public function getBlockPrefix()
+  {
+    return $this->getName();
+  }
 }
--- src/Sasedev/Commons/BootstrapBundle/SasedevCommonsBootstrapBundle.php
+++ src/Sasedev/Commons/BootstrapBundle/SasedevCommonsBootstrapBundle.php
@@ -1,9 +1,12 @@
 <?php
-
 namespace Sasedev\Commons\BootstrapBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ *
+ * @author sasedev <seif.salah@gmail.com>
+ */
 class SasedevCommonsBootstrapBundle extends Bundle
 {
 }
